bb0feec300d831e52c9a23e543ee8c43c7e93084
KDr2 <zhuo@hexoasis.com>
2016-01-12 11:05:32 +0800

    init commit, use glisp as a s-exp parser and interpreter

---
+++ test/dval_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/appwilldev/Instafig/dynval"
+	"github.com/stretchr/testify/assert"
+	"github.com/zhemao/glisp/interpreter"
+	"testing"
+)
+
+func TestStub(t *testing.T) {
+	assert.True(t, true, "This is good. Canary test passing")
+}
+
+func TestDvalTruncate(t *testing.T) {
+	env := glisp.NewGlisp()
+	dv := dynval.NewDynValFromString("(+ 1 2)(* 4 5)", env)
+	assert.True(t, dv.Sexp_str == "(+ 1 2)")
+}
