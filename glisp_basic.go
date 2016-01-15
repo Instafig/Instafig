5e1af8fe1e2d0758464490b2398c600e0b0ce25c
KDr2 <zhuo@hexoasis.com>
2016-01-15 10:59:33 +0800

    version compare functions

--- glisp_basic.go
+++ glisp_basic.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"github.com/hashicorp/go-version"
 	"github.com/zhemao/glisp/interpreter"
 )
 
@@ -9,8 +11,56 @@ func defmacroCondValues(env *glisp.Glisp) {
 	env.EvalString(macro)
 }
 
+// version-cmp
+func VersionCompareFunction(env *glisp.Glisp, name string,
+	args []glisp.Sexp) (glisp.Sexp, error) {
+	if len(args) != 2 {
+		return glisp.SexpNull, glisp.WrongNargs
+	}
+
+	var vleft, vright string
+
+	switch t := args[0].(type) {
+	case glisp.SexpStr:
+		vleft = string(t)
+	default:
+		return glisp.SexpNull, errors.New("version argument must be string")
+	}
+
+	switch t := args[1].(type) {
+	case glisp.SexpStr:
+		vright = string(t)
+	default:
+		return glisp.SexpNull, errors.New("version argument must be string")
+	}
+
+	v1, err := version.NewVersion(vleft)
+	if err != nil {
+		return glisp.SexpNull, errors.New("version format error")
+	}
+	v2, err := version.NewVersion(vright)
+	if err != nil {
+		return glisp.SexpNull, errors.New("version format error")
+	}
+	res := v1.Compare(v2)
+	return glisp.SexpInt(res), nil
+}
+
+func defVersionCompareFunctions(env *glisp.Glisp) {
+	env.AddFunction("version-cmp", VersionCompareFunction)
+	shortcuts := `
+         (defn ver= [v1 v2] (= (version-cmp v1 v2) 0))
+         (defn ver> [v1 v2] (> (version-cmp v1 v2) 0))
+         (defn ver< [v1 v2] (< (version-cmp v1 v2) 0))
+         (defn ver>= [v1 v2] (>= (version-cmp v1 v2) 0))
+         (defn ver<= [v1 v2] (<= (version-cmp v1 v2) 0))
+    `
+	env.EvalString(shortcuts)
+}
+
 func NewGlisp() *glisp.Glisp {
 	env := glisp.NewGlisp()
 	defmacroCondValues(env)
+	defVersionCompareFunctions(env)
 	return env
 }
--- glisp_basic_test.go
+++ glisp_basic_test.go
@@ -12,3 +12,22 @@ func TestMacroCondValues(t *testing.T) {
 	ret, _ := env.EvalString("(cond-values false 1 true 2 3)")
 	assert.True(t, ret == glisp.SexpInt(2))
 }
+
+func TestVersionCompareFunctions(t *testing.T) {
+	env := NewGlisp()
+	var ret glisp.Sexp
+	ret, _ = env.EvalString(`(ver= "1.1.1" "1.1.1")`)
+	assert.True(t, ret == glisp.SexpBool(true))
+	ret, _ = env.EvalString(`(ver= "1.1.1" "1.1.2")`)
+	assert.True(t, ret == glisp.SexpBool(false))
+	ret, _ = env.EvalString(`(ver= "1.3" "1.3.0")`)
+	assert.True(t, ret == glisp.SexpBool(true))
+	ret, _ = env.EvalString(`(ver< "1.1.1" "1.1.2")`)
+	assert.True(t, ret == glisp.SexpBool(true))
+	ret, _ = env.EvalString(`(ver< "1.3.1" "1.3")`)
+	assert.True(t, ret == glisp.SexpBool(false))
+	ret, _ = env.EvalString(`(ver> "1.1.1" "1.1.2")`)
+	assert.True(t, ret == glisp.SexpBool(false))
+	ret, _ = env.EvalString(`(ver> "1.3.1" "1.3")`)
+	assert.True(t, ret == glisp.SexpBool(true))
+}
