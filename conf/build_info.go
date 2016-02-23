197ea7bc982135e7c2daf366d7539fae69b610df
rahuahua <rahuahua@gmail.com>
2016-02-23 17:06:33 +0800

    use runtime.GOOS&runtime.GOARCH to compose VERSION_BUILD_INFO

--- conf/build_info.go
+++ conf/build_info.go
@@ -1,12 +1,11 @@
 package conf
 
 import (
-	"runtime"
 	"fmt"
+	"runtime"
 )
 
 var VERSION_BUILD_INFO = "build.dev-darwin-x86_64"
-
 
 func init() {
 	VERSION_BUILD_INFO = fmt.Sprintf("%s-%s", runtime.GOOS, runtime.GOARCH)
