f08bd2c7e4f62d8f0645537153205a4e8df30212
rahuahua <rahuahua@gmail.com>
2016-03-08 14:24:03 +0800

    minor code refactor

--- statistic.go
+++ statistic.go
@@ -2,50 +2,30 @@ package main
 
 import (
 	"log"
-	//"os/exec"
-	//"os"
+	"os"
 	"time"
-	//"path/filepath"
 
 	"github.com/appwilldev/Instafig/conf"
 	"github.com/gin-gonic/gin"
 	influx "github.com/influxdata/influxdb/client/v2"
-
-	"os"
 )
 
 var (
 	statisticCh       = make(chan *influx.Point, 100000)
 	influxClient      influx.Client
 	influxBatchPoints influx.BatchPoints
-	//logf *os.File
-
+	microSecondUnit   = time.Microsecond / time.Nanosecond
 )
 
 func init() {
 	if conf.StatisticEnable {
-		//cpath, err := os.Getwd()
-		//if err != nil {
-		//	log.Println("Failed to get current path:", err.Error())
-		//	os.Exit(1)
-		//}
-		//logPath := filepath.Join(cpath, "statistic_log")
-		//if err := exec.Command("mkdir", "-p", logPath).Run(); err != nil {
-		//	log.Println("Failed to create statistic log dir in current path: ", err.Error())
-		//	os.Exit(1)
-		//}
-		//
-		//logf, err = os.OpenFile(filepath.Join(logPath, "statistic.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-		//if err != nil {
-		//	log.Println("Failed to create statistic log file: ", err.Error())
-		//	os.Exit(1)
-		//}
 		var err error
-		influxClient, err = influx.NewHTTPClient(influx.HTTPConfig{
-			Addr:     conf.InfluxURL,
-			Username: conf.InfluxUser,
-			Password: conf.InfluxPassword,
-		})
+		influxClient, err = influx.NewHTTPClient(
+			influx.HTTPConfig{
+				Addr:     conf.InfluxURL,
+				Username: conf.InfluxUser,
+				Password: conf.InfluxPassword,
+			})
 		if err != nil {
 			log.Println("Failed to init influx client: ", err.Error())
 			os.Exit(1)
@@ -86,7 +66,7 @@ func StatisticHandler(c *gin.Context) {
 		"osv":        clientData.OSVersion,
 		"appv":       clientData.AppVersion,
 		"deviceid":   clientData.DeviceId,
-		"resp_time":  int(respTime / (time.Microsecond / time.Nanosecond)),
+		"resp_time":  int(respTime / microSecondUnit),
 	}
 
 	p, _ := influx.NewPoint("client_request", tags, fields, time.Now())
@@ -119,10 +99,10 @@ func logStatistic(p *influx.Point) {
 
 	if err := influxClient.Write(influxBatchPoints); err != nil {
 		log.Println("Failed to dump point to influxdb: ", err.Error())
-	} else {
-		influxBatchPoints, _ = influx.NewBatchPoints(influx.BatchPointsConfig{
+	}
+	influxBatchPoints, _ = influx.NewBatchPoints(
+		influx.BatchPointsConfig{
 			Database:  conf.InfluxDB,
 			Precision: "s",
 		})
-	}
 }
